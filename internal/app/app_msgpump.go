acfec534659c2581a72afa92e2c07299ada956b2
Andrew Lee <andyrewlee@gmail.com>
2026-01-23 04:48:15 -0800

    Preserve PTY messages under backpressure

--- internal/app/app_msgpump.go
+++ internal/app/app_msgpump.go
@@ -1,11 +1,7 @@
 package app
 
 import (
-	"time"
-
 	tea "charm.land/bubbletea/v2"
-
-	"github.com/andyrewlee/amux/internal/logging"
 )
 
 func (a *App) SetMsgSender(send func(tea.Msg)) {
@@ -25,11 +21,7 @@ func (a *App) enqueueExternalMsg(msg tea.Msg) {
 	if msg == nil || a.externalMsgs == nil {
 		return
 	}
-	select {
-	case a.externalMsgs <- msg:
-	default:
-		a.logExternalDrop()
-	}
+	a.externalMsgs <- msg
 }
 
 func (a *App) drainExternalMsgs() {
@@ -39,16 +31,4 @@ func (a *App) drainExternalMsgs() {
 		}
 		a.externalSender(msg)
 	}
-}
-
-func (a *App) logExternalDrop() {
-	now := time.Now().UnixNano()
-	last := a.externalDropLastLog.Load()
-	if now-last < int64(time.Second) {
-		return
-	}
-	if !a.externalDropLastLog.CompareAndSwap(last, now) {
-		return
-	}
-	logging.Warn("External message queue full; dropping PTY messages")
 }
